81535c459195395fa1f49e54639e8eb245e13d10
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2023-10-30 21:30:54 +0800

    Add lsp settings

--- config/nvim/lua/ypaskell/packer.lua
+++ config/nvim/lua/ypaskell/packer.lua
@@ -57,9 +57,8 @@ packer.startup(function(use)
         branch = 'v3.x',
         requires = {
             --- Uncomment these if you want to manage LSP servers from neovim
-            -- {'williamboman/mason.nvim'},
-            -- {'williamboman/mason-lspconfig.nvim'},
-
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
             -- LSP Support
             {'neovim/nvim-lspconfig'},
             -- Autocompletion
