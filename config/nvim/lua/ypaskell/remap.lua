b1d6c8a10af50ba84477e818a1ce3feed87fc92d
ypprog.us01 <pan.yiping.fi@gmail.com>
2023-11-03 14:52:44 +0800

    Add raw search

--- config/nvim/lua/ypaskell/remap.lua
+++ config/nvim/lua/ypaskell/remap.lua
@@ -19,3 +19,14 @@ map("n", "<F12>", [[:%s/\s\+$//e<cr>]])
 
 -- Nerdtree
 map("n", "<F2>", ":NERDTreeToggle<CR>")
+
+-- Raw stirng search :SS <\foo>
+vim.cmd([[
+    command! -nargs=1 SS let @/ = '\V'.escape(<q-args>, '\')|set hlsearch
+
+    if &term =~ "ansi"
+        let &t_ti = "\<Esc>[?47h"
+        let &t_te = "\<Esc>[?47l"
+    endif
+    ]]
+)
---
+++ config/nvim/tips.md
@@ -0,0 +1,9 @@
+# Tips
+
+## Search selected text
+
+1. yank
+2. `/`
+3. `<C-r>`
+4. `"`
+5. `<enter>`
