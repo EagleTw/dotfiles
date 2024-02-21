f29b341dc82d1b9dc10baa47296bdf1325faaa41
ypprog.us01 <pan.yiping.fi@gmail.com>
2024-02-21 09:24:54 +0800

    Indent file

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -1,20 +1,20 @@
 require'nvim-treesitter.configs'.setup {
-  -- 安装 language parser
-  -- :TSInstallInfo 命令查看支持的语言
-  ensure_installed = {"c", "cpp", "python", "rust"},
+    -- 安装 language parser
+    -- :TSInstallInfo 命令查看支持的语言
+    ensure_installed = {"c", "cpp", "python", "rust"},
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-  -- 启用代码高亮功能
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false
-  },
-  -- 启用增量选择
-  -- 启用基于Treesitter的代码格式化(=) . NOTE: This is an experimental feature.
-  indent = {
-    enable = true
-  }
+    -- 启用代码高亮功能
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false
+    },
+    -- 启用增量选择
+    -- 启用基于Treesitter的代码格式化(=) . NOTE: This is an experimental feature.
+    indent = {
+        enable = true
+    }
 }
