3d96e1643e4c6078dc80522da8e87ae61f6a6a3d
ypprog.us01 <pan.yiping.fi@gmail.com>
2024-03-07 11:54:17 +0800

    Add md format

--- config/nvim/lua/vesijarvi/core.lua
+++ config/nvim/lua/vesijarvi/core.lua
@@ -52,6 +52,15 @@ vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGai
   pattern = { "*" },
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+        callback = function()
+          vim.opt.shiftwidth = 2
+          vim.opt.tabstop = 2
+          vim.opt.softtabstop = 2
+    end,
+})
+
 
 -- Highlight stuffs
 o.list = true
