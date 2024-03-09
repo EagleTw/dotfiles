a7ada62bedd69d2aac98810135f229853844d730
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2024-03-09 19:46:57 +0800

    Replace " with '

--- config/nvim/lua/vesijarvi/remap.lua
+++ config/nvim/lua/vesijarvi/remap.lua
@@ -7,25 +7,24 @@ local function map(m, k, v)
 end
 
 -- Mimic shell movements
-map("i", "<C-E>", "<ESC>A")
-map("i", "<C-A>", "<ESC>I")
+map('i', '<C-E>', '<ESC>A')
+map('i', '<C-A>', '<ESC>I')
 
-map("n", "<leader>hs", "<C-w>s")             -- Horizontal split
-map("n", "<leader>vs", "<C-w>v")             -- Vertical split
+map('n', '<leader>hs', '<C-w>s')             -- Horizontal split
+map('n', '<leader>vs', '<C-w>v')             -- Vertical split
 map('n', '<Leader>wt', [[:%s/\s\+$//e<cr>]]) -- Remove trailing whitespace
 
 -- Split movements
-map("n", "<leader>wh", "<C-w>h")
-map("n", "<leader>wj", "<C-w>j")
-map("n", "<leader>wk", "<C-w>k")
-map("n", "<leader>wl", "<C-w>l")
+map('n', '<leader>wh', '<C-w>h')
+map('n', '<leader>wj', '<C-w>j')
+map('n', '<leader>wk', '<C-w>k')
+map('n', '<leader>wl', '<C-w>l')
 
 -- Self
-map("n", "<F2>", ":NERDTreeToggle<CR>")       -- Nerdtree
-map("n", "<leader>nt", ":NERDTreeToggle<CR>") -- Nerdtree
-map("n", "<F3>", ":SymbolsOutline<CR>")       -- Outline
-map("n", "<leader>srn", ":set rnu!<CR>")      -- Toogle reletive number
-
+map('n', '<F2>', ':NERDTreeToggle<CR>')       -- Nerdtree
+map('n', '<leader>nt', ':NERDTreeToggle<CR>') -- Nerdtree
+map('n', '<F3>', ':SymbolsOutline<CR>')       -- Outline
+map('n', '<leader>srn', ':set rnu!<CR>')      -- Toogle reletive number
 
 -- Raw stirng search :SS <\foo>
 vim.cmd([[
