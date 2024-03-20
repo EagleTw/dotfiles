3beb8f19e36e773ad83720d852613885457dbbc1
ypprog.us01 <pan.yiping.fi@gmail.com>
2024-03-19 23:01:13 -0700

    Replace o. with vim.o

--- config/nvim/lua/vesijarvi/core.lua
+++ config/nvim/lua/vesijarvi/core.lua
@@ -5,48 +5,45 @@
 -------------------------------------------------
 -- BASE SETTING
 -------------------------------------------------
-local g = vim.g
-local o = vim.o
-local opt = vim.opt
-
 --cmd('syntax on')
 --vim.api.nvim_command('filetype plugin indent on')
 
-o.termguicolors = true
-o.background = 'dark'
+vim.o.termguicolors = true
+vim.o.background = 'dark'
 
 -- Do not save when switching buffers
--- o.hidden = true
+-- vim.o.hidden = true
 
 -- Decrease update time
-o.timeoutlen = 500
-o.updatetime = 100
+vim.o.timeoutlen = 500
+vim.o.updatetime = 100
 
 -- Number of screen lines to keep above and below the cursor
-o.scrolloff = 8
+vim.o.scrolloff = 8
 
 -- Better editor UI
-o.number = true
-o.relativenumber = false
-o.numberwidth = 2
-o.signcolumn = "yes"
-o.cursorline = true
-o.colorcolumn = "80"
+vim.o.number = true
+vim.o.relativenumber = false
+vim.o.numberwidth = 2
+vim.o.signcolumn = "yes"
+vim.o.cursorline = true
+vim.o.colorcolumn = "80"
 
 -- Better editing experience
-o.expandtab = true
-o.smarttab = true
-o.cindent = true
-o.autoindent = true
-o.wrap = true
-o.textwidth = 300
-o.tabstop = 8
-o.shiftwidth = 4
-o.softtabstop = -1 -- If negative, shiftwidth value is used
+vim.o.smarttab = true
+vim.o.cindent = true
+vim.o.autoindent = true
+vim.o.wrap = true
+vim.o.textwidth = 300
+
+vim.o.expandtab = true
+vim.o.tabstop = 8
+vim.o.shiftwidth = 4
+vim.o.softtabstop = -1 -- If negative, shiftwidth value is used
 
 -- auto-reload files when modified externally
 -- https://unix.stackexchange.com/a/383044
-o.autoread = true
+vim.o.autoread = true
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
   command = "if mode() != 'c' | checktime | endif",
   pattern = { "*" },
@@ -58,60 +55,60 @@ vim.api.nvim_create_autocmd("FileType", {
             vim.opt.shiftwidth = 2
             vim.opt.tabstop = 2
             vim.opt.softtabstop = 2
-            vim.expandtab = false
-            vim.cmd.colorscheme("github_dark_tritanopia")
+            vim.g.expandtab = false
+            --vim.cmd.colorscheme("github_dark_tritanopia")
     end,
 })
 
 
 -- Highlight stuffs
-o.list = true
-o.listchars = "eol:$,trail:★,tab:» "
-o.formatoptions = 'qrn1'
+vim.o.list = true
+vim.o.listchars = "eol:$,trail:★,tab:» "
+vim.o.formatoptions = 'qrn1'
 
 vim.cmd("highlight EoLSpace ctermbg=DarkBlue guibg=DarkBlue")
 vim.cmd("match EoLSpace /\\s\\+$/")
 
 -- Makes neovim and host OS clipboard play nicely with each other
-o.clipboard = "unnamedplus"
+vim.o.clipboard = "unnamedplus"
 
 -- Case insensitive searching UNLESS /C or capital in search
-o.ignorecase = true
-o.smartcase = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
 -- Undo and backup options
-o.backup = false
-o.writebackup = false
-o.undofile = true
-o.swapfile = false
--- o.backupdir = '/tmp/'
--- o.directory = '/tmp/'
--- o.undodir = '/tmp/'
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.undofile = true
+vim.o.swapfile = false
+-- vim.o.backupdir = '/tmp/'
+-- vim.o.directory = '/tmp/'
+-- vim.o.undodir = '/tmp/'
 
 -- Remember 50 items in commandline history
-o.history = 50
+vim.o.history = 50
 
 -- Better buffer splitting
-o.splitright = true
-o.splitbelow = true
+vim.o.splitright = true
+vim.o.splitbelow = true
 
 -- Preserve view while jumping
 -- BUG This option causes an error!
--- o.jumpoptions = 'view'
+-- vim.o.jumpoptions = 'view'
 
 -- BUG: this won't update the search count after pressing `n` or `N`
 -- When running macros and regexes on a large file, lazy redraw tells neovim/vim not to draw the screen
--- o.lazyredraw = true
+-- vim.o.lazyredraw = true
 
 -- Better folds (don't fold by default)
--- o.foldmethod = 'indent'
--- o.foldlevelstart = 99
--- o.foldnestmax = 3
--- o.foldminlines = 1
-opt.mouse = "a"
+-- vim.o.foldmethod = 'indent'
+-- vim.o.foldlevelstart = 99
+-- vim.o.foldnestmax = 3
+-- vim.o.foldminlines = 1
+vim.opt.mouse = "a"
 
 -- Map <leader> to space
 --vim.keymap.set("n", " ", "<Nop>", { silent = true, remap = true })
-g.mapleader = " "
-g.maplocalleader = " "
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
