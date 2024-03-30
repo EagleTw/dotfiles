0bca596ae3b2e52b1f2dfe361c7de6c0c55455c4
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2024-03-30 19:50:55 +0800

    Revert "Update"
    This reverts commit 5bc4ce43af391180907cee35b1d937d1be33378b.

--- config/nvim/lua/vesijarvi/core.lua
+++ config/nvim/lua/vesijarvi/core.lua
@@ -42,7 +42,6 @@ vim.opt.softtabstop = -1 -- If negative, shiftwidth value is used
 
 vim.opt.list = true
 vim.opt.listchars = "eol:$,trail:★,tab:» "
---vim.opt.listchars = "trail:★,tab:» "
 vim.opt.formatoptions = 'qrn1'
 
 vim.o.backup = false
@@ -57,10 +56,10 @@ vim.cmd("match EoLSpace /\\s\\+$/")
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "markdown",
         callback = function()
-            vim.opt.shiftwidth = 4
-            vim.opt.tabstop = 4
-            vim.opt.softtabstop = 4
-            vim.g.expandtab = true
+            vim.opt.shiftwidth = 2
+            vim.opt.tabstop = 2
+            vim.opt.softtabstop = 2
+            vim.g.expandtab = false
             --vim.cmd.colorscheme("github_dark_tritanopia")
     end,
 })
