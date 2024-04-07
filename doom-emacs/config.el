72785fc2fb177d0209c680a00348b9af3725627f
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2024-04-07 18:10:31 +0800

    Update doom setting

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -7,7 +7,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Yi-Ping Pan"
-      user-mail-address "yipingp@synopsys.com")
+      user-mail-address "yipingp@outlook.com")
 
 ;; Auto maximized when open
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
