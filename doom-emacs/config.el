ceba7c389b663dbca7681b5e56a8ee7e0370b2b2
yipingp.us01 <pan.yiping.fi@gmail.com>
2024-04-09 09:37:55 +0800

    Add hl-todo config

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -35,6 +35,21 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+(use-package hl-todo
+  :hook ((prog-mode . hl-todo-mode)
+         (markdown-mode . hl-todo-mode)
+         (org-mode . hl-todo-mode))
+  :config
+  (setq hl-todo-highlight-punctuation ":"
+        hl-todo-keyword-faces
+        `(("TODO"       warning bold)
+          ("FIXME"      error bold)
+          ("HACK"       font-lock-constant-face bold)
+          ("REVIEW"     font-lock-keyword-face bold)
+          ("DONE"       success bold)
+          ("NOTE"       success bold)
+          ("DEPRECATED" font-lock-doc-face bold))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Org-mode
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
