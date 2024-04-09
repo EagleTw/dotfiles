0bcc7c9734ea3470b0b3ebb7cb982d2f9c60cb70
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2024-04-09 19:08:02 +0800

    Modify default screen size

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -10,7 +10,29 @@
       user-mail-address "yipingp@outlook.com")
 
 ;; Auto maximized when open
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
+;; (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Bryan Oakley's settings.
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+  (progn
+    ;; use 120 char wide window for largeish displays
+    ;; and smaller 80 column windows for smaller displays
+    ;; pick whatever numbers make sense for you
+    (if (> (x-display-pixel-width) 1280)
+           (add-to-list 'default-frame-alist (cons 'width 100))
+           (add-to-list 'default-frame-alist (cons 'width 80)))
+    ;; for the height, subtract a couple hundred pixels
+    ;; from the screen height (for panels, menubars and
+    ;; whatnot), then divide by the height of a char to
+    ;; get the height we want
+    (add-to-list 'default-frame-alist
+         (cons 'height (/ (- (x-display-pixel-height) 200)
+                             (frame-char-height)))))))
+
+(set-frame-size-according-to-resolution)
+
 
 ;; line number
 (global-display-line-numbers-mode)
@@ -28,7 +50,7 @@
 ;; Appearence
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq doom-font (font-spec :family "IntelOne Mono" :"size 12"))
+(setq doom-font (font-spec :family "IntelOne Mono" :"size 15"))
 (setq doom-theme 'doom-oceanic-next)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
